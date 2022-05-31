ea20783681321054219011c4d6e5fddcde933918
kiran <kirangummadi1985@gmail.com>
2022-05-31 23:23:07 +0530

    roboshop-jenkins

---
+++ vars/ansible.groovy
@@ -0,0 +1,42 @@
+def call() {
+    pipeline {
+        agent any
+
+        options {
+            ansiColor('xterm')
+
+        }
+
+        parameters {
+            string(name: 'ENV', defaultValue: '', description: 'Which environment')
+            string(name: 'COMPONENT', defaultValue: '', description: 'Which component')
+        }
+
+        environment {
+            SSH = credentials('SSH')
+        }
+
+        stages {
+
+            stage('Create Instance') {
+                steps {
+                    script {
+                        ec2InstanceCreate.create("${COMPONENT}", "${ENV}")
+                    }
+                }
+            }
+
+            stage('Run Ansible Playbook') {
+                steps {
+                    sh 'ansible-playbook -i inv roboshop.yml -e ENV=${ENV} -e ansible_user=${SSH_USR} -e ansible_password=${SSH_PSW} -e HOST=${COMPONENT} -e ROLE_NAME=${COMPONENT}'
+                }
+            }
+
+
+
+        }
+
+
+    }
+
+}
