8af5cef077f48b42450f2dc08133e584ca5cb338
kiran <kirangummadi1985@gmail.com>
2022-06-01 01:01:38 +0530

    roboshop-jenkins

--- vars/ansible.groovy
+++ vars/ansible.groovy
@@ -18,17 +18,46 @@ def call() {
 
         stages {
 
+            stage('CheckOut Code - Dev') {
+                when {
+                    expression {
+                        ENV == "dev"
+                    }
+                }
+                steps {
+                    dir('ANSIBLE') {
+                        git branch: 'dev', url: 'https://github.com/kirangummadi1985/roboshop-ansible.git'
+                    }
+                }
+            }
+
+            stage('CheckOut Code - Prod') {
+                when {
+                    expression {
+                        ENV == "prod"
+                    }
+                }
+                steps {
+                    dir('ANSIBLE') {
+                        git branch: 'main', url: 'https://github.com/kirangummadi1985/roboshop-ansible.git'
+                    }
+                }
+            }
+
+
             stage('Create Instance') {
                 steps {
-                    script {
-                        ec2InstanceCreate.create("${COMPONENT}", "${ENV}")
+                    dir('ANSIBLE') {
+                        sh 'ec2InstanceCreate.create("${COMPONENT}", "${ENV}")'
                     }
                 }
             }
 
             stage('Run Ansible Playbook') {
                 steps {
-                    sh 'ansible-playbook -i inv roboshop.yml -e ENV=${ENV} -e ansible_user=${SSH_USR} -e ansible_password=${SSH_PSW} -e HOST=${COMPONENT} -e ROLE_NAME=${COMPONENT}'
+                    dir('ANSIBLE') {
+                        sh 'ansible-playbook -i inv roboshop.yml -e ENV=${ENV} -e ansible_user=${SSH_USR} -e ansible_password=${SSH_PSW} -e HOST=${COMPONENT} -e ROLE_NAME=${COMPONENT}'
+                    }
                 }
             }
 
